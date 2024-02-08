51e29ef97b8fdc07213551af4f147219f0063f92
Martin Šimůnek <public@e-mis.cz>
2024-02-08 19:05:20 +0100

    Ukázka řazení seznamu
    Základní řazení s využitím
    `Comparator.comparting(...)`.
    
    Ukázka použití `.reversed()`.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import cz.engeto.ja.Computer;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -18,5 +20,19 @@ public class Main {
         for (Computer computer : computers) {
             System.out.println(computer.getDescription() + " " + computer.getYearOfProduction() + " " + computer.getPrice());
         }
+
+        computers.sort(Comparator.comparing(Computer::getYearOfProduction).reversed());
+
+        System.out.println("\nSorted by year of production:\n");
+        computers.forEach(System.out::println);
+//        for (Computer computer : computers) {
+//            System.out.println(computer);
+//        }
+
+        // Obecnější pro jakoukoli kolekci: Collections.sort(computers, Comparator.comparing(Computer::getPrice));
+        computers.sort(Comparator.comparing(Computer::getPrice));
+
+        System.out.println("\nSorted by price from lowest:\n");
+        computers.forEach(System.out::println);
     }
 }

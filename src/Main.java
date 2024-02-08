f447abd490cfbeee555788bc9af1721354e5fd38
Martin Šimůnek <public@e-mis.cz>
2024-02-08 18:35:46 +0100

    Testovací seznam ve třídě Main
    Je připraven pro ukázku řazení seznamu.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import cz.engeto.ja.Computer;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Computer> computers = new ArrayList<>();
+
+        Computer computer1 = new Computer("Dell", 2019, 15000);
+        Computer computer2 = new Computer("HP", 2018, 12000);
+        Computer computer3 = new Computer("Lenovo", 2017, 10000);
+
+        computers.add(computer1);
+        computers.add(computer2);
+        computers.add(computer3);
+
+        for (Computer computer : computers) {
+            System.out.println(computer.getDescription() + " " + computer.getYearOfProduction() + " " + computer.getPrice());
+        }
     }
 }
